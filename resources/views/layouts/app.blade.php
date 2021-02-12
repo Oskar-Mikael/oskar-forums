6b0c2fccc54a07d002b8cfc6e7fd3c134caa8641
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-12 16:02:18 +0100

    Add links to new JS and CSS files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,9 +20,13 @@
     <!-- Styles -->
     <link rel="stylesheet"
           href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet"
+          href="{{ asset('css/main.css') }}">
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"
+            defer></script>
+    <script src="{{ asset('js/main.js') }}"
             defer></script>
 
     <!-- jQuery -->
