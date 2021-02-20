ce7bcb9370d2472ccbdddb82003b485630f3bdb8
Oskar-Mikael <oskar.bostrom@chasacademy.se>
2021-02-20 17:20:53 +0100

    Initial commit

---
+++ resources/js/components/MessageComponent.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-text="Test"
+    id="threads-delete-message"
+    class="text-2xl text-white text-center mt-20 bg-green-500 w-1/2 mx-auto py-1 rounded border-black border-2"
+  >
+    
+    <button
+      id="threads-delete-message-button"
+      class="float-right mr-5 outline-none">
+      X
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+    name: "MessageComponent",
+}
+
+</script>
